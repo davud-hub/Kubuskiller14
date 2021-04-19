4fe111b0d503c5869f2596a48d49bd7a9dd296a2
BOTjoker-ops <stijnvdpouw@gmail.com>
2021-04-19 13:07:45 +0200

    fixed JSON

---
+++ main.js
@@ -0,0 +1,23 @@
+const data = fetch("competeties.json")
+                .then(response => response.json())
+                .then(data => {
+                    document.querySelector("#id").innerText = data.JSONid
+                },
+
+
+function showData(tab, subTab) {
+for (let i = 0; i < len(data.tab.subTab); i++){
+    <div class="c-card">
+        <div class="c-header">
+            data.tab.subTab.i.Key
+        </div>
+        <div class="c-body">
+            <div>data.tab.subTab.i.Value</div>
+        </div>
+    </div>
+}
+}
+
+function beroepsTaken(taak, laag){
+    document.querySelector("#beroepstaken").innerText = data.beroepstaken.//classid
+}              
