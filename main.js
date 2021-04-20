2c26b2f47bcbeef4fe760cbe02a2e55af9fefec8
BOTjoker-ops <stijnvdpouw@gmail.com>
2021-04-20 14:58:16 +0200

    JSON-function

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-$(document).ready(){
-    $.get("competenties.json")
-}
+$(document).ready(function() {
+    $.get("competenties.json", function( data, status){
+        
+        var data=JSON.parse(data);
+
+        data.forEach(function(card){
+            $("#contain-box").append(
+            '<div class="c-card">' +
+                '<div class="c-header">' +
+                    card.title +
+                '</div>' +
+                '<div class="c-body">' +
+                    '<div>' +
+                        card.text +
+                    '</div>' +
+                '</div>' +
+            '</div>'
+            )
+        })
+    })
+})
