5e84c0568bd3c7b35df815fe863fdd3188a55101
BOTjoker-ops <stijnvdpouw@gmail.com>
2021-04-21 12:17:33 +0200

    error overload

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
-    $.get("competenties.json", function( data, status){
-        
-        var data=JSON.parse(data);
-
-        data.forEach(function(card){
-            $("#contain-box").append(
-            '<div class="c-card">' +
-                '<div class="c-header">' +
-                    card.title +
-                '</div>' +
-                '<div class="c-body">' +
-                    '<div>' +
-                        card.text +
+    fetch("competenties.json")
+        .then(response => response.json()
+        .then(data => {
+            console.log(data)
+            data["profesionalskills"]["subTab"].forEach(function(card){
+                $("#profskills").append(
+                    '<div class="contain">' + 
+                    '<div class="c-card">' +
+                        '<div class="c-header">' +
+                            card.title +
+                        '</div>' +
+                        '<div class="c-body">' +
+                            '<div>' +
+                                card.text +
+                            '</div>' +
+                        '</div>' +
                     '</div>' +
-                '</div>' +
-            '</div>'
-            )
-        })
+                    '</div>'
+                )
+            })
+
+        }))
     })
-})
