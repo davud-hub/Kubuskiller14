3fd267d6cf959ef85acd88f8df19fdb44eb6e8a2
peacock | mac <stijnvdpouw@gmail.com>
2021-04-23 10:49:21 +0200

    fixed

--- main.js
+++ main.js
@@ -2,21 +2,25 @@ $(document).ready(function() {
     fetch("competenties.json")
         .then(response => response.json()
         .then(data => {
-            console.log(data)
-            data["profesionalskills"]["subTab"].forEach(function(card){
-                $("#profskills").append(
-                    '<div class="contain">' + 
-                    '<div class="c-card">' +
-                        '<div class="c-header">' +
-                            card.title +
-                        '</div>' +
-                        '<div class="c-body">' +
-                            '<div>' +
-                                card.text +
+            console.log(data["professionalskills"][0])
+            data["professionalskills"].forEach( skill => {
+                skill.cards.forEach( card => {
+                    $("#doelgericht").append(
+                        '<div class="c-card">' +
+                            '<div class="c-header">' +
+                                card.title +
+                            '</div>' +
+                            '<div class="c-body">' +
+                                '<div>' +
+                                    card.text +
+                                '</div>' +
                             '</div>' +
                         '</div>' +
-                    '</div>' +
-                    '</div>'
+                        '</div>'
+                    )
+
+
+                }
                 )
             })
 
