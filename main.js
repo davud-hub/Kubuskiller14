2b4d835ee0cbf659f81c4f8610c363e8a9ecf4fb
BOTjoker-ops <stijnvdpouw@gmail.com>
2021-04-23 11:48:33 +0200

    grote insect

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
     fetch("competenties.json")
         .then(response => response.json()
-        .then(data => {
+            .then(data => {
 
-            function showCards (tab, subTab) {
+                data["professionalskills"].forEach(tabs => {
 
-            }
+                    tabs.cards.forEach(card => {
 
-            console.log(data["professionalskills"][0]["cards"])
-            data["professionalskills"][0]["cards"].forEach( card => {
-                    $("#doelgericht").append(
-                        '<div class="c-card">' +
+                        $("#" + tabs.tabTitle).append(
+                            '<div class="c-card">' +
                             '<div class="c-header">' +
-                                card.title +
+                            card.title +
                             '</div>' +
                             '<div class="c-body">' +
-                                '<div>' +
-                                    card.text +
-                                '</div>' +
+                            '<div>' +
+                            card.text +
                             '</div>' +
-                        '</div>' +
-                        '</div>'
-                    )
+                            '</div>' +
+                            '</div>' +
+                            '</div>'
+                        )
+
+                    })
+
+                })
 
 
-                }
-                )
-            
+            }))
 
-        }))
-    })
+
+        })
