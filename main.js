fca290fa507b6d6d30388b1f90249beef2b44fbd
TarikTorgaddon <sven.vanderzouw@student.hu.nl>
2021-04-29 16:21:01 +0200

    beroepstaken almost done

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-$(document).ready(function () {
+function initialise() {
+    showdata("beroepstaken");
+    showdata("beheersingniveaus");
+    showdata("professionalskills");
+}
+
+function showdata(tab){
     fetch("competenties.json")
-        .then(response => response.json()
-            .then(data => {
-
-                data["professionalskills"].forEach(tabs => {
-
-                    tabs.cards.forEach(card => {
-                        console.log("#" + tabs.tabTitle)
-                        $("#" + tabs.tabTitle).append(
-                            '<div class="c-card">' +
-                            '<div class="c-header">' +
-                            card.title +
-                            '</div>' +
-                            '<div class="c-body">' +
-                            '<div>' +
-                            card.text +
-                            '</div>' +
-                            '</div>' +
-                            '</div>' +
-                            '</div>'
+    .then(response => response.json()
+        .then(data => {
+            data[tab].forEach(tabs => {
+                tabs.cards.forEach(card => {
+                    console.log("#" + tabs.tabTitle)
+                    $("#" + tabs.tabTitle).append(
+                        '<div class="c-card">' +
+                        '<div class="c-header">' +
+                        card.title +
+                        '</div>' +
+                        '<div class="c-body">' +
+                        '<div>' +
+                        card.text +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
                         )
-
-                    })
-
                 })
-
-
-            }))
-
-
+            })
         })
+    )
+}
+
+$(document).ready(initialise);
